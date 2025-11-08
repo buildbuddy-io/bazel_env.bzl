27eea6957e6a6468b27e42c95dc484d01c1c4d47
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-11-08 18:45:11 +0100

    Update Java and Jar version checks in script

--- examples/bazel_env_test.sh
+++ examples/bazel_env_test.sh
@@ -143,8 +143,8 @@ case "$(arch)" in
   *) goarch="$(arch)";;
 esac
 assert_cmd_output "go version" "go version go1.21.13 $(uname|tr '[:upper:]' '[:lower:]')/$goarch"
-assert_cmd_output "jar --version" "jar 17.0.14"
-assert_cmd_output "java --version" "openjdk 17.0.14 2025-01-21 LTS"
+assert_cmd_output "jar --version" "jar 17.0.17"
+assert_cmd_output "java --version" "openjdk 17.0.17 2025-01-21 LTS"
 assert_cmd_output "jq --version" "jq-1.7"
 assert_cmd_output "node --version" "v16.18.1"
 assert_cmd_output "pnpm --version" "8.6.7"

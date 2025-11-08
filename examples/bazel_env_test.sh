7b3b366320e5726cf7f6057b30e3678a0b60cb2c
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-11-08 21:19:59 +0100

    Update bazel_env_test.sh

--- examples/bazel_env_test.sh
+++ examples/bazel_env_test.sh
@@ -166,7 +166,7 @@ assert_cmd_output "terraform --version" "Terraform v1.9.3"
 if [ "$TOOLCHAIN_TYPES_SUPPORTED" = true ]; then
   assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/go/bin/go version" "go version go1.21.13 $(uname|tr '[:upper:]' '[:lower:]')/$goarch"
 fi
-assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/jdk/bin/java --version" "openjdk 17.0.14 2025-01-21 LTS"
+assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/jdk/bin/java --version" "openjdk 17.0.17 2025-10-21 LTS"
 assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/python/bin/python3 --version" "Python 3.11.8"
 assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/rust/bin/cargo --version" "cargo 1.80.0 (376290515 2024-07-16)"
 assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/rust/bin/rustc --version" "rustc 1.80.0 (051478957 2024-07-21)"

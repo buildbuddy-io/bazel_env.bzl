2ef09964977f23f57d891d11fae5f8e0c3f5b6be
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-09-24 16:00:30 +0200

    Update test deps for last_green (#121)

--- examples/bazel_env_test.sh
+++ examples/bazel_env_test.sh
@@ -215,8 +215,8 @@ case "$(arch)" in
   *) goarch="$(arch)";;
 esac
 assert_cmd_output "go version" "go version go1.21.13 $(uname|tr '[:upper:]' '[:lower:]')/$goarch"
-assert_cmd_output "jar --version" "jar 17.0.17"
-assert_cmd_output "java --version" "openjdk 17.0.17 2025-10-21 LTS"
+assert_cmd_output "jar --version" "jar 17.0.20"
+assert_cmd_output "java --version" "openjdk 17.0.20 2026-07-21 LTS"
 assert_cmd_output "jq --version" "jq-1.7"
 assert_cmd_output "node --version" "v16.18.1"
 assert_cmd_output "pnpm --version" "8.6.7"
@@ -261,7 +261,7 @@ assert_cmd_output "loc_tool" "found: *location_test_data*"
 if [ "$TOOLCHAIN_TYPES_SUPPORTED" = true ]; then
   assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/go/bin/go version" "go version go1.21.13 $(uname|tr '[:upper:]' '[:lower:]')/$goarch"
 fi
-assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/jdk/bin/java --version" "openjdk 17.0.17 2025-10-21 LTS"
+assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/jdk/bin/java --version" "openjdk 17.0.20 2026-07-21 LTS"
 assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/python/bin/python3 --version" "Python 3.11.8"
 assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/rust/bin/cargo --version" "cargo 1.80.0 (376290515 2024-07-16)"
 assert_cmd_output "$build_workspace_directory/bazel-out/bazel_env-opt/bin/bazel_env/toolchains/rust/bin/rustc --version" "rustc 1.80.0 (051478957 2024-07-21)"
